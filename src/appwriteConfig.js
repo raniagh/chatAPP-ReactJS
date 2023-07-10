17702df5c613919361be19728b53e38bd1df8536
Your Name <rghzaiel@ggn-group.com>
2023-07-10 12:26:55 +0100

    first commit

---
+++ src/appwriteConfig.js
@@ -0,0 +1,15 @@
+import { Client, Databases } from "appwrite";
+
+export const PROJECT_ID = "64abbc56bc67b3bac0dd";
+export const DATABASE_ID = "64abc27d3de0f10e4a84";
+export const COLLECTION_ID_MESSAGES = "64abc284883595cbcaad";
+
+const client = new Client();
+
+client
+  .setEndpoint("https://cloud.appwrite.io/v1")
+  .setProject("64abbc56bc67b3bac0dd");
+
+export const databases = new Databases(client);
+
+export default client;
