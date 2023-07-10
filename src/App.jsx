3da6aa20b2f86570906ff6dd8da4ea33d93391ba
Your Name <rghzaiel@ggn-group.com>
2023-07-10 13:28:32 +0100

    add private routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
 import Room from "./pages/Room";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import PrivateRoutes from "./components/PrivateRoutes";
 
 function App() {
   return (
-    <>
-      <Room />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route element={<PrivateRoutes />}>
+          <Route path="/" element={<Room />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
